570016f216ffaf7195eee4ecdf207356e3dc5fc8
Vincent Humeau <hello@vincent-humeau.com>
2018-06-12 22:07:12 +0100

    Started basic logic

---
+++ content/js/customs/app.js
@@ -0,0 +1,37 @@
+//https://rickandmortyapi.com/documentation
+// https://rickandmortyapi.com/api/character
+document.addEventListener('DOMContentLoaded', function(){ 
+    var app = new Vue({
+        el: '#invoice-app',
+        data: {
+            invoiceCurrency: "£",
+            products: [
+                { description: 'product1', quantity: 1, price: 300 },
+                { description: 'product2', quantity: 1, price: 25 },
+                { description: 'product3', quantity: 1, price: 10 },
+                { description: 'product4', quantity: 3, price: 50 }
+            ]
+        },
+        methods: {
+            addNewProduct: function() {
+                this.products.push(
+                    { description: '', quantity: 0, price: 0 }
+                )
+            }
+        },
+        computed: {
+            grandTotal: function() {
+                var total = this.products.reduce(function(accumulator, product) {
+                    return accumulator + (product.price * product.quantity);
+                }, 0)
+
+                return total;
+            }
+        },
+        filters: {
+            currency: function(value) {
+                return value.toFixed(2);
+            }
+        }
+    });
+}, false);
